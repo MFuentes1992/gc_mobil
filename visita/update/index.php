92bba6a2c91007ab30765a8c74fd569208ac6151
Marco Fuentes <marco.fuentes.teseracto@smartbusinesscorp.com>
2024-04-20 13:20:52 -0600

    Endpoint to update visita

---
+++ visita/update/index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once $_SERVER['DOCUMENT_ROOT']."/model/VisitaModel.php";
+    require_once $_SERVER['DOCUMENT_ROOT']."/constants/constants.php";
+    require_once $_SERVER['DOCUMENT_ROOT']."/sessionManager/SessionManager.php"; 
+    session_start();
+    $payload = $_POST;
+    $session = new SessionManager();
+    $connValues = $session->getSession("userConn");
+    $model = new Visit($connValues["dbUrl"], $connValues["user"], $connValues["password"], $connValues["dbName"]); 
+    
+    $res = $model->updateVisita(intval($payload["idVisita"]), intval($payload["tipoVisita"]), intval($payload["tipoIngreso"]),
+    $payload["fechaIngreso"], $payload["fechaSalida"],
+    intval($payload["multiEntrada"]), intval($payload["notificaciones"]), $payload["nombreVisita"],
+    1,
+    isset($payload["vehicles"]) ? $payload["vehicles"] : "",);
+    if($res) {
+        header("HTTP/1.1 200 OK");
+        $msg = array("estatus"=> "200", "message"=>"La visita se actualizo correctamente");
+        echo json_encode($msg); 
+    } else {
+        header("HTTP/1.1 400 ERROR");
+        $msg = array("estatus"=> "400", "message"=>"Algo salio mal, intente de nuevo.", );
+        echo json_encode($msg);    
+    }
+?>
