0c47cb20785dbff82a9405c01fb2f437a3546b87
MFuentes1992 <markfuentes2991@gmail.com>
2024-07-03 15:02:30 -0600

    Change credentials for release v1.0

--- constants/constants.php
+++ constants/constants.php
@@ -1,10 +1,10 @@
 <?php
-    $baseUrl = "http://localhost:8081";
-    // $baseUrl = "https://apimovilgc.dasgalu.net";
+    // $baseUrl = "http://localhost:8081";
+    $baseUrl = "https://movil.gestioncomunidad.mx";
     $dbUrl = "109.106.251.202";
-    $dbName = "u579469339_GC_admins";
-    $dbUser = "u579469339_GC_admins";
-    $dbPass = "8!San^MEj";
+    $dbName = "u579469339_gc_admins_prod";
+    $dbUser = "u579469339_gc_admins_prod";
+    $dbPass = "7mYK+g;<0g82";
     $dbSession = "userConn";
     $dbPrefix = "u579469339";
     $fcmProjectId = "gcmobile-5d55a";
