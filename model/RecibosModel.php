b04c119553e2ec4539508653c9ddc936ebe34044
MFuentes1992 <markfuentes2991@gmail.com>
2024-07-12 13:39:36 -0600

    Recibos endpoint

---
+++ model/RecibosModel.php
@@ -0,0 +1,19 @@
+<?php 
+    require_once $_SERVER['DOCUMENT_ROOT']."/db/Connection.php";
+
+    class Recibos extends Connection {
+        function __construct($dbUrl, $dbUser, $dbPass, $dbName) {
+            parent::__construct($dbUrl, $dbUser, $dbPass, $dbName);
+        }
+
+        function getRecibosByInstalacionResidente(int $residenteId, int $instalacionId, int $recintoId) {
+            try {
+                $query = sprintf("SELECT * FROM recibos WHERE id_residente = %d AND id_instalacion = %d AND id_recinto = %d", $residenteId, $instalacionId, $recintoId);
+                return $this->execQuery($query);
+            } catch (\Throwable $th) {
+                echo $th;
+            }
+        }
+    }
+
+?>
