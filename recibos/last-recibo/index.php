80167c7ea5566ed9bc1f73460c4b756954f5dbe0
MFuentes1992 <markfuentes2991@gmail.com>
2024-08-12 21:44:53 -0600

    Get Last recibo.

---
+++ recibos/last-recibo/index.php
@@ -0,0 +1,36 @@
+<?php 
+require_once $_SERVER['DOCUMENT_ROOT'] . "/model/RecibosModel.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/constants/constants.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/sessionManager/SessionManager.php"; 
+session_start();
+$residenteId = $_GET["residenteId"];
+$instalacionId = $_GET["instalacionId"];
+$recintoId = $_GET["recintoId"];
+if (isset($residenteId) && isset($instalacionId) && isset($recintoId)) {
+    $session = new SessionManager();
+    $connValues = $session->getSession("userConn");
+    $recibosModel = new Recibos($connValues["dbUrl"], $connValues["user"], $connValues["password"], $connValues["dbName"]);
+    $res = $recibosModel->getRecibosByInstalacionResidenteLast($residenteId, $instalacionId, $recintoId);
+    $attachmentsArr = array();
+    while ($row = $res->fetch_array()) {
+        array_push($attachmentsArr, array(
+            "id" => $row["id"],
+            "nombre" => $row["archivo"],
+            "fecha" => $row["fecha_elaboracion"],
+            "folio" => $row["folio"],              
+        ));
+    }
+    $response = array(
+        "status" => "OK",
+        "attachments" => $attachmentsArr
+    );
+    echo json_encode($response);
+} else {
+    header("HTTP/1.1 400 ERROR");
+    $response = array(
+        "status" => "ERROR",
+        "message" => "Parámetros incorrectos"
+    );
+    echo json_encode($response);
+}
+?>
