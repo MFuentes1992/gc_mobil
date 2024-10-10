755179de0d821499595675aab2548a1f0cd2476e
MFuentes1992 <markfuentes2991@gmail.com>
2024-10-10 13:52:56 -0600

    Logs by caseta

---
+++ repository/VigilanteRepository.php
@@ -0,0 +1,45 @@
+<?php 
+    require_once $_SERVER['DOCUMENT_ROOT']."/db/Connection.php";
+    require_once $_SERVER['DOCUMENT_ROOT']."/model/response/LogsGetAllResponse.php";
+
+    Class VigilanteRepository extends Connection {
+
+        function __construct($dbUrl, $dbUser, $dbPass, $dbName) {
+            parent::__construct($dbUrl, $dbUser, $dbPass, $dbName);
+        }
+
+        public function getAllRegisteredLogsByCasetaId(int $casetaId) {
+            try {
+                $query = sprintf("SELECT 
+                    v.id as id_visita, 
+                    b.id as id_bitacora, 
+                    i.id as id_instalacion, 
+                    i.seccion, i.numero, 
+                    v.nombre_visita, 
+                    b.fecha_lectura 
+                    FROM bitacora_visita b 
+                    INNER JOIN visitas v 
+                    ON b.id_visita = v.id 
+                    INNER JOIN instalaciones i 
+                    ON v.id_instalacion = i.id 
+                    INNER JOIN info_caseta_vigilancia cv
+                    ON b.id_caseta = cv.id
+                    WHERE b.id_caseta = %d AND cv.estatus_registro = 1", $casetaId);
+                $resQuery = $this->execQuery($query);
+                $logs = array();
+                if($resQuery && $resQuery->num_rows > 0) {
+                    while($row = $resQuery->fetch_array()) {
+                        $log = new LogsGetAllResponse($row["id_visita"], $row["id_bitacora"], $row["id_instalacion"], $row["seccion"], $row["numero"], $row["nombre_visita"], $row["fecha_lectura"]);
+                        array_push($logs, $log);
+                    }
+                }
+                return $logs;
+            } catch (\Throwable $th) {
+                //throw $th;
+                echo $th;
+            }
+        }
+
+    }
+
+?>
--- service/VigilantesService.php
+++ service/VigilantesService.php
@@ -1,20 +1,43 @@
 <?php 
     require_once $_SERVER['DOCUMENT_ROOT']."/model/VigilantesModel.php";
     require_once $_SERVER['DOCUMENT_ROOT']."/constants/constants.php";
-    require_once $_SERVER['DOCUMENT_ROOT']."/sessionManager/SessionManager.php"; 
+    require_once $_SERVER['DOCUMENT_ROOT']."/sessionManager/SessionManager.php";
+    require_once $_SERVER['DOCUMENT_ROOT']."/repository/VigilanteRepository.php";
+
 
     Class VigilanteService {
         private $model;
+        private $vigilanteRepository;
+
         function __construct() {
             session_start();
             $session = new SessionManager();
             $connValues = $session->getSession("userConn");
             $this->model = new Vigilante($connValues["dbUrl"], $connValues["user"], $connValues["password"], $connValues["dbName"]);        
+            $this->vigilanteRepository = new VigilanteRepository($connValues["dbUrl"], $connValues["user"], $connValues["password"], $connValues["dbName"]);
         }
 
         public function getInfoCaseta(string $activationCode) {
             $res = $this->model->getCasetaInformation($activationCode);
             return $res;
+        }
+
+        public function getLogsByCasetaId(int $casetaId) {
+            $res = $this->vigilanteRepository->getAllRegisteredLogsByCasetaId($casetaId);            
+            $arrayRes = array();
+            foreach($res as $log) {
+                $log->setFechaLectura(date("Y-m-d H:i:s", strtotime($log->getFechaLectura())));
+                array_push($arrayRes,array(
+                    "id_visita" => $log->getIdVisita(),
+                    "id_bitacora" => $log->getIdBitacora(),
+                    "id_instalacion" => $log->getIdInstalacion(),
+                    "seccion" => $log->getSeccion(),
+                    "numero" => $log->getNumero(),
+                    "nombre_visita" => $log->getNombreVisita(),
+                    "fecha_lectura" => $log->getFechaLectura()
+                ));
+            }
+            return $arrayRes;
         }
     }
 ?>
