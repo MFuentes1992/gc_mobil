8b36ee36bc10ad4ece9a5cc58acef0c49bf88c6a
MFuentes1992 <markfuentes2991@gmail.com>
2024-11-23 20:39:25 -0600

    QA deploy script

--- pipeline-deploy/qa-deploy.sh
+++ pipeline-deploy/qa-deploy.sh
@@ -3,3 +3,10 @@
 git checkout main
 // pull latest changes
 git pull origin main
+cd ../constants
+rm constants.php
+cp /QA/constants.php constants.php
+cd ../
+scp -r . u579469339.dasgalu_net@dasgalu.net:/api_movil_gc/
+git checkout .
+echo "Successfully deployed to QA!"
